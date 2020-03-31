0dbf487a29263f6399d18eb9a150d54d78b43dcf
RhinoCodes <51250021+RhinoCodes@users.noreply.github.com>
2020-03-31 14:41:43 -0500

    Add the files

---
+++ t.py
@@ -0,0 +1,11 @@
+from web import *
+from random import randint
+createindex()
+createpage('joe')
+header('index', 'BRALALALALALAALALALALALALA', True)
+piclink('index','joe' ,'pop.png')
+header('joe', 'Player: JOE')
+header('joe', 'Score: '+str(randint(100000, 999999999999999999999999999999999999999999)), True)
+title('index', 'BRALALALALALAALALALALALALA')
+title('joe', 'BRALALALALALAALALALALALALA - JOE')
+start('index')
